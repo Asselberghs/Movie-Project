ea9bb6d97c5671b6405a005b6ee78d31a8b6af2a
Nick Asselberghs <nick@asselberghs.dk>
2015-01-22 13:27:15 +0100

    Open Source GNU GPL Version 3 Added

--- mainmenu.php
+++ mainmenu.php
@@ -1,3 +1,20 @@
 <?php
+/*
+    This is a media database to mange your Movies.
+    Copyright (C) 2013 Nick Tranholm Asselberghs
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
    echo '<a href="http://www.asselberghs.dk">Hjem</a> &#124; <a href="http://www.asselberghs.dk/familiens-blog/">Familiens Blog</a> &#124; <a href="http://www.asselberghs.dk/cv/">C.V.</a> &#124; <a href="http://www.asselberghs.dk/Movie/">Film Database</a> &#124; <a href="http://www.asselberghs.dk/Game/">Spil Database</a> &#124; <a href="http://www.asselberghs.dk/Book/">Bog Database</a> &#124; <a href="http://www.linkedin.com/pub/nick-asselberghs/41/572/38b" target="_blank">Linkedin</a> &#124; <a href="http://photos.asselberghs.dk/">Asselberghs Web Album</a> &#124; <a href="http://en.asselberghs.dk/">Engelsk</a>';
 ?>
